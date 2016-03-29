ebea546bab3a64fdb04897bf886236035cb28cbc
Sunny Yaying Zhang <sunnyzhangyaying@gmail.com>
2016-03-29 15:04:18 -0700

    improve the skeleton data structure after the change of server side

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ $(function(){
     // btns for capturing initial poses into pools.
     var btnCaptL = $("#btnCaptureL");
     var btnCaptR = $("#btnCaptureR");
+    // btn for generation
+    var btnGenerate = $("#btnGenerate");
+    // btn to restart capturing
+    var btnRestart = $("#btnRestart");
 
 	// set up websocket
 	var socket;
@@ -27,7 +31,7 @@ $(function(){
     };
     socket.onmessage = function (event) {
         // console.log(event.data);
-        var bodies = JSON.parse(event.data);
+        var bodies = JSON.parse(event.data)[0];
         updatePose(bodies);
         drawPose(curPose, previewFrameContext, previewFrameWidth, previewFrameHeight);
     }; //socket.onmessage ends
@@ -39,11 +43,57 @@ $(function(){
     });
     btnCaptR.on("click", function(){
     	capturePose(curPose, poolDPoses);
+        redrawPool(poolDPoses, "poolDad");
+    });
+    btnGenerate.on("click", function(){
+        $("#preview").hide();
+        $("#poolBabe").show();
+        if(poolMPoses.length==0 || poolDPoses.length==0){
+            alert("Please capture pose first.");
+            return;
+        }
+        generatePoses();
+    });
+    btnRestart.on("click", function(){
+        poolMPoses = [];
+        poolDPoses = [];
+        redrawPool(poolMPoses, "poolMom");
+        redrawPool(poolMPoses, "poolDad");
+
+        $("#poolBabe").hide();
+        $("#preview").show();
     });
 
 });
 
-// the current pose that we capture.
+// define the skeloton names in the data
+var SPINEBASE   = 0;
+var SPINEMID    = 1;
+var NECK        = 2;
+var HEAD        = 3;
+var SHOULDERLEFT = 4;
+var ELBOWLEFT   = 5;
+var WRISTLEFT   = 6;
+var HANDLEFT    = 7;
+var SHOULDERRIGHT = 8; 
+var ELBOWRIGHT  = 9;
+var WRISTRIGHT  = 10;
+var HANDRIGHT   = 11;
+var HIPLEFT     = 12;
+var KNEELEFT    = 13;
+var ANKLELEFT   = 14;
+var FOOTLEFT    = 15;
+var HIPRIGHT    = 16;
+var KNEEDRIGHT  = 17;
+var ANKLERIGHT  = 18;
+var FOORRIGHT   = 19;
+var SPINESHOULDER = 20;
+var HANDTIPLEFT = 21;
+var THUMBLEFT   = 22;
+var HANDTIPRIGHT = 23;
+var THUMBRIGHT  = 24;
+
+// the current pose that we are capturing.
 var curPose;
 // the poses in poolMom and poolDad
 var poolMPoses = [];
@@ -53,21 +103,20 @@ var savedPoses = [];
 // update current pose when websocket send a new pose
 var updatePose = function(newPose){
 	curPose = newPose;
+    // console.log(curPose);
 };
 // draw pose in the context
 var drawPose = function(pose, cxt, canvW, canvH){
-    for (var i = 0; i < curPose.length; i++) {
         cxt.clearRect(0, 0, canvW, canvH);
     	
     	cxt.fillStyle = "#31B131";
-        var jointDictionary = pose[i];
+        var jointDictionary = pose;
         for (var joint in jointDictionary) {
             var jnt = jointDictionary[joint];
-            var x = jnt.X;
-            var y = jnt.Y;
+            var x = jnt.X * 1000 + 500;
+            var y = jnt.Y * -1000 + 500;
             cxt.fillRect(x - 8, y - 8, 16, 16);
         }
-    }
 };
 // capture curPose into pools. pool = poolMPoses or poolDPoses.
 var capturePose = function(pose, pool){
@@ -80,13 +129,11 @@ var savePose = function(pose){
 // redraw the pool - used when the content of pools changes
 var redrawPool = function(pool, poolDOMid){
     var poolDom = $("#"+poolDOMid);
+    poolDom.html("");
     var poolPosesDom = poolDom.find(".poolPose");
     for(var i=0; i<pool.length; i++)
     {
-        if (i>=poolPosesDom.length)
-        { 
-            poolDom.append('<canvas class="poolPose" width="1920" height="1080"></canvas>');
-        }
+        poolDom.append('<canvas class="poolPose" width="1920" height="1080"></canvas>');
         var poolPosesDom = poolDom.find(".poolPose");
         var cnt = poolPosesDom.eq(i).get(0);
         var canvW = cnt.width;
@@ -96,3 +143,25 @@ var redrawPool = function(pool, poolDOMid){
     }
     console.log(poolDOMid + " updated.")
 }
+
+// generate 10 poses
+var generatePoses = function(){
+    for(var i=0; i<10; i++){
+        var pose1 = poolMPoses[parseInt(Math.random()*poolMPoses.length)];
+        var pose2 = poolMPoses[Math.random(poolDPoses.length)];
+        generatePose(pose1, pose2);
+    }
+};
+// generate a new pose from two parent poses
+var generatePose = function(mom, dad){
+    var kid; 
+    return kid;
+};
+
+
+
+
+
+
+
+
